efdcfd8af9eea94aea8153981e2866dba3980efe
richard smarsi <ricsmarsi@gmail.com>
2021-11-10 12:05:54 -0300

    Alterações no projeto Fusion, adicionada funcionalidade do formulário de contato. Alterado o arquivo hero.html para inserir alert de sucesso ou erro ao enviar o e-mail.

--- fusion/core/views.py
+++ fusion/core/views.py
@@ -1,20 +1,32 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Servico, Funcionario, Features
+from .forms import ContatoForm
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html' #Template view só precisa que indiquemos o nome do template que será usado para funcionar.
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
+
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         features = Features.objects.all()
         features_sort = [features[i::2] for i in range(2)]
 
-        print(f'Esquerda {features_sort[0]}')
-        print(f'Direita {features_sort[1]}')
-
         context['servicos'] = Servico.objects.order_by('?').all()
         context['funcionarios'] = Funcionario.objects.order_by('?').all()
         context['features_l'] = features_sort[0]
         context['features_r'] = features_sort[1]
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
